ac32459973bc9db2957bfe57a267a43d28b10f74
Ian Reddy <73981521+GamyIan@users.noreply.github.com>
2025-07-29 09:36:12 +0000

    Added Practicals 5 and 6

---
+++ Practicals/Practical5/Practical5.java
@@ -0,0 +1,48 @@
+public class Practical5 {
+	/*
+	Q1. Write a program to convert every alternate word of a given sentence into uppercase and display the modified sentence.
+	Q2. Write a program which has a method for longest word, which extracts the longest word from the sentence.
+	 */
+	
+	String q1(String sentence) {
+		String output="";
+		String[] words=sentence.split(" ");
+		int len=words.length;
+		for (int i=0;i<len;i++) {
+			if (i%2==0) {
+				output+=words[i];
+			}
+			else {
+				output+=words[i].toUpperCase();
+			}
+            output+=" ";
+		}
+		return output;
+	}
+	
+	String q2(String sentence) {
+		String[] words=sentence.split(" ");
+		String longest=words[0];
+		int longlen=words[0].length();
+		
+		for (String word : words) {
+			int len=word.length();
+			if (len > longlen) {
+				longest=word;
+				longlen=len;
+			}
+		}
+		
+		return longest;
+		
+	}
+	
+	public static void main(String[] args) {
+		Practical5 p5=new Practical5();
+		String a1=p5.q1("Make alternate words in this sentence UpperCase.");
+		System.out.println("Answer Q1:\n"+a1);
+		String a2=p5.q2("Find the longest word from this sentence.");
+		System.out.println("Answer Q2:\n"+a2);
+	}
+	
+}
---
+++ Practicals/Practical6/Practical6.java
@@ -0,0 +1,52 @@
+/*
+Q3. Write a program which has method display() which shows only the center word from the sentence.
+Q4. (Tough) You are given a sentence. Filter out the following words (“the”,"these",”all”,”them”).
+ */
+
+public class Practical6 {
+	String display(String sentence) {
+		String output="";
+		String[] words=sentence.split(" ");
+		int len=words.length;
+        if (len%2==0)
+        {
+            int mid=(len/2)-1;
+            output+=words[mid]+", "+words[mid+1];
+        }
+        else {
+            int mid=((len+1)/2)-1;
+            output+=words[mid];
+        }
+
+		return output;
+	}
+	
+	String q2(String sentence, String[] blacklist) {
+        String replaced="";
+        String[] words=sentence.split(" ");
+        for (String word : words){
+            boolean contains=false;
+            for (String blackword : blacklist){
+                if (word.equalsIgnoreCase(blackword)){
+                    contains=true;
+                    break;
+                }
+            }
+            if (!contains){
+                replaced+=word+" ";
+            }
+
+        }
+		return replaced;
+	}
+	
+	public static void main(String[] args) {
+		Practical6 p6=new Practical6();
+		String a3=p6.display("Get the Center word from this sentence.");
+		System.out.println("Answer Q1:\n"+a3);
+        String[] black={"the","these","all","them"};
+		String a4=p6.q2("Remove certain words like the word the these them and all. All of these words are supposed to be in the blacklist",black);
+		System.out.println("Answer Q2:\n"+a4);
+	}
+	
+}
